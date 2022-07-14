e04de46b3fee79bc379e2284465b9c22060735fc
omarelsayed23 <108306241+omarelsayed23@users.noreply.github.com>
2022-07-15 00:45:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,33 +20,8 @@ resource "aws_instance" "web" {
 
 }
 
-resource "aws_instance" "temp1" {
 
-	ami           = "ami-098e42ae54c764c35"
-	instance_type = "t2.micro"
-
-  tags = {
-    Name = "temp1"
-  }
-
-
-}
-
-resource "aws_instance" "twist" {
-
-
-	ami           = "ami-098e42ae54c764c35"
-	instance_type = "t2.micro"
-
-  tags = {
-    Name = "twist"
-  }
-
-
-}
-
-
-resource "aws_instance" "temp2" {
+resource "aws_instance" "temp" {
   count = 0
 	ami           = "ami-098e42ae54c764c35"
 	instance_type = "t2.micro"
@@ -59,7 +34,7 @@ resource "aws_instance" "temp2" {
         type        = "ssh"
         user        = "ec2-user"
         private_key = file("omar-key.pem")
-        host        = aws_instance.twist.public_ip
+        host        = aws_instance.web.public_ip
         timeout     = "2m"
     }
 
@@ -70,7 +45,6 @@ resource "aws_instance" "temp2" {
     "wget -O go_downloaded https://go.dev/dl/go1.18.4.linux-amd64.tar.gz",
     "sudo tar -C /usr/local -xzf go_downloaded",
     "echo \"export PATH=$PATH:/usr/local/go/bin\" >> ~/.bashrc",
-    "sudo reboot",
     "sudo yum groupinstall Development Tools -y",
     "go install github.com/NethermindEth/juno/cmd/juno@latest",
     "go install github.com/NethermindEth/juno/cmd/juno-cli@latest",
