e30908677e18a8416b207ac923d0a5aeb324887f
omarelsayed23 <108306241+omarelsayed23@users.noreply.github.com>
2022-07-21 06:06:18 +0200

    Update main.tf

--- cloud-init-approach/main.tf
+++ cloud-init-approach/main.tf
@@ -19,19 +19,21 @@ provider "aws" {
   region = "us-west-2"
 }
 
+
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQChYHQ7etlb6LdPkou+A/vjVvrUcnTIB2r6yNFe9Uo+Fiw71f7NpQhZEmYusV5Hpl3zxo6OLYJL4q6Bo8kqsppu0rQawRd9OHguiWrm9bOvlbHpcDU7czslRh+1WoVulLrrHl31JfeLATF2hOpHXE5V9UM2aPAMfCFHTGxJG7C5Yfw74trdZ8NqjCU4aUUvcjvCexzDtil64X0OU1sfMf2EBHGoV0pxU0iRLuAECFkzDEJrl45QBmUBMli9+35VTJec8MykO5boEQCL/CulJhXwSFGe28t6dLpfAr7y3Rioq+KkRQe5Pja6oH+WxBy87GQ8wJauKWGzOs8/IipMbqTl"
+  public_key = var.my_pub_key
 }
 
 
 data "template_file" "user_data" {
-  template = "${file("./userdata.yaml")}"
+  template = "${file("./userdata8.yaml")}"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-098e42ae54c764c35"
-  instance_type = "t2.micro"
+resource "aws_instance" "web2" {
+  
+  ami           = var.ami_value
+  instance_type = "t2.large"
   key_name = "${aws_key_pair.deployer.key_name}"
   user_data = data.template_file.user_data.rendered
   vpc_security_group_ids = [aws_security_group.sg_web.id]
@@ -45,8 +47,9 @@ resource "aws_instance" "web" {
 }
 
 
-output "public_ip" {
-  value = aws_instance.web.public_ip
+output "public_ip" {  
+  description = "Public IP address of the EC2 instance"
+  value = aws_instance.web2.public_ip
 }
 
 data "aws_vpc" "main" {
@@ -76,7 +79,7 @@ resource "aws_security_group" "sg_web" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["197.54.175.194/32"]
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = []
     prefix_list_ids = []
     security_groups = []
